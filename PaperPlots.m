9d13eb0a4d6c8806447a68d48b3838ecc346b95c
Javier Concha <jaconcha@gmail.com>
2015-03-31 17:23:33 -0400

    new maps with lat and lon

--- PaperPlots.m
+++ PaperPlots.m
@@ -1,10 +1,16 @@
 cd /Users/javier/Desktop/Javier/PHD_RIT/ConferencesAndApplications/2015_Landsat_Special_Issue/Images
 %% Plot ROI 
-% clear
-% clc
+clear
+clc
 
 folderpath = '/Users/javier/Desktop/Javier/PHD_RIT/LDCM/L8images/';
-filename = 'LC80170302014272LGN00/LC80170302014272LGN00_ROI_RGB.tif';
+% filename = 'LC80170302014272LGN00/LC80170302014272LGN00_ROI_RGB.tif';
+filename = 'LC80160302013262LGN00/LC80160302013262LGN00_ONelm140629.tif';
+
+% filename = 'LC80160302013262LGN00/LC80160302013262LGN00_WaterAndDTROC.tif';
+% filename = 'LC80160302013262LGN00/LC80160302013262LGN00_WholeImage.tif';
+
+
 
 filepath = [folderpath filename];
 %%
@@ -41,27 +47,38 @@ R.Lonlim = [min(lonProj(:)) max(lonProj(:))];
 R.ColumnsStartFrom = 'north';
 R.RowsStartFrom = 'west';
 
-folderpath = '/Users/javier/Desktop/Javier/PHD_RIT/LDCM/L8images/';
-filename = 'LC80170302014272LGN00/LC80170302014272LGN00_ROI_RGB.tif';
-filepath = [folderpath filename];
+% folderpath = '/Users/javier/Desktop/Javier/PHD_RIT/LDCM/L8images/';
+% filename = 'LC80170302014272LGN00/LC80170302014272LGN00_ROI_RGB.tif';
+% filepath = [folderpath filename];
 
 [RGB,map] = imread(filepath);
 
-RGB_R = double(RGB(:,:,3));
-RGB_G = double(RGB(:,:,2));
-RGB_B = double(RGB(:,:,1));
+RGB_R = double(RGB(:,:,4));
+RGB_R(RGB_R<0)=0;
+
+RGB_G = double(RGB(:,:,3));
+RGB_G(RGB_G<0)=0;
+
+RGB_B = double(RGB(:,:,2));
+RGB_B(RGB_B<0)=0;
 
 % Adjusting threshold for display
-CTE = 1.5;
+CTE = 1.5; % constant
 
-threshold_B = mean(RGB_B(:))+CTE*std(RGB_B(:));
-RGB_B(RGB_B>threshold_B)=threshold_B;
+threshold_Bpos = mean(RGB_B(:))+CTE*std(RGB_B(:));
+RGB_B(RGB_B>threshold_Bpos)=threshold_Bpos;
+threshold_Bneg = mean(RGB_B(:))-CTE*std(RGB_B(:));
+RGB_B(RGB_B<threshold_Bneg)=threshold_Bneg;
 
-threshold_G = mean(RGB_G(:))+CTE*std(RGB_G(:));
-RGB_G(RGB_G>threshold_G)=threshold_G;
+threshold_Gpos = mean(RGB_G(:))+CTE*std(RGB_G(:));
+RGB_G(RGB_G>threshold_Gpos)=threshold_Gpos;
+threshold_Gneg = mean(RGB_G(:))-CTE*std(RGB_G(:));
+RGB_G(RGB_G<threshold_Gneg)=threshold_Gneg;
 
-threshold_R = mean(RGB_R(:))+CTE*std(RGB_R(:));
-RGB_R(RGB_R>threshold_R)=threshold_R;
+threshold_Rpos = mean(RGB_R(:))+CTE*std(RGB_R(:));
+RGB_R(RGB_R>threshold_Rpos)=threshold_Rpos;
+threshold_Rneg = mean(RGB_R(:))-CTE*std(RGB_R(:));
+RGB_R(RGB_R<threshold_Rneg)=threshold_Rneg;
 
 % Convert values to [0 1] for display
 RGBdisplay_B = (RGB_B-min(RGB_B(:)))/(max(RGB_B(:))-min(RGB_B(:)));
@@ -78,21 +95,24 @@ set(gcf,'color','white')
 set(gca,'fontsize',fs)
 % ax = worldmap(R.Latlim,R.Lonlim);
 
+sep = 0.05;
+% sep = 0.5;
+
 ax = axesm(mstruct, 'Grid', 'on',...
     'GColor', [.9 .9 .9], ...
     'MapProjection','utm',...
     'MapLatlimit', R.Latlim, 'MapLonLimit', R.Lonlim, ...
-    'ParallelLabel', 'on', 'PLabelLocation', .025, 'PlabelMeridian', 'west', ...
-    'MeridianLabel', 'on', 'MlabelLocation', .05, 'MLabelParallel', 'south', ...
+    'ParallelLabel', 'on', 'PLabelLocation', sep, 'PlabelMeridian', 'west', ...
+    'MeridianLabel', 'on', 'MlabelLocation', sep, 'MLabelParallel', 'south', ...
     'MLabelRound', -2, 'PLabelRound', -2, ...
-    'PLineVisible', 'on', 'PLineLocation', .025, ...
-    'MLineVisible', 'on', 'MlineLocation', .05, ...
+    'PLineVisible', 'on', 'PLineLocation', sep, ...
+    'MLineVisible', 'on', 'MlineLocation', sep, ...
     'Grid','off', ...
     'Frame','on', ...
     'FontWeight','bold', ...
-    'FontSize',18, ...
+    'FontSize',24, ...
     'LabelUnits','degrees');
-
+%
 geoshow(RGBdisplay, R)
 
 axis off
@@ -124,10 +144,11 @@ PIFrad140929   	= ELMpar(:,4);
 RrsDTROC4272 	= ELMpar(:,5);
 
 figure
+% subplot(1,2,1)
 fs = 15;
 lw = 1.5;
 set(gcf,'color','white')
-plot(ELMpar(:,1),Darkrad140929,'k','LineWidth',lw)
+plot(ELMpar(:,1),Darkrad140929,'--k','LineWidth',lw)
 hold on 
 plot(ELMpar(:,1),PIFrad140929 ,'LineWidth',lw)
 legend('Dark: water ROI','Bright: PIF from L8')
@@ -139,10 +160,11 @@ xlim([.4 2.5])
 grid on
 
 figure
+% subplot(1,2,2)
 fs = 15;
 lw = 1.5;
 set(gcf,'color','white')
-plot(ELMpar(:,1),ONTOSRef_140919,'k','LineWidth',lw)
+plot(ELMpar(:,1),ONTOSRef_140919,'--k','LineWidth',lw)
 hold on 
 plot(ELMpar(:,1),RrsDTROC4272,'LineWidth',lw)
 legend('Dark: ONTOS field','Bright: PIF L8 refl. product')
@@ -155,14 +177,13 @@ grid on
 
 %% ROIs 
 ROIstat = [...
-  0.443000  0.009901  0.010249  0.010188  0.010691  0.010247  0.032758;
-  0.482600  0.009343  0.009598  0.009578  0.010056  0.010751  0.040786;
-  0.561300  0.007739  0.013285  0.013020  0.019612  0.015439  0.059070;
-  0.654600  0.003437  0.008682  0.008326  0.012264  0.008745  0.072488;
-  0.864600  0.001848  0.003361  0.003281  0.003740  0.003287  0.092126;
-  1.609000  0.000699  0.000749  0.000777  0.000946  0.000744  0.115534;
-  2.201000  0.000065  0.000056  0.000089  0.000254  0.000077  0.117337];
-
+  0.443000   0.005023   0.005443   0.005369   0.005977   0.005441   0.032622;
+  0.482600   0.007257   0.007529   0.007508   0.008020   0.008765   0.040927;
+  0.561300   0.008571   0.014000   0.013740   0.020194   0.016108   0.058821;
+  0.654600   0.001785   0.007206   0.006838   0.010907   0.007271   0.073146;
+  0.864600   0.000069   0.001626   0.001545   0.002017   0.001550   0.093038;
+  1.609000   0.000011   0.000062   0.000090   0.000261   0.000057   0.116034;
+  2.201000  -0.000001  -0.000011   0.000023   0.000188   0.000011   0.117406];
 wl    = ROIstat(:,1);
 ONTOS = ROIstat(:,2);
 LONGS = ROIstat(:,3);
@@ -176,10 +197,10 @@ figure
 fs = 15;
 lw = 1.5;
 set(gcf,'color','white')
-plot(ROIstat(:,1),ROIstat(:,2:end),'LineWidth',lw)
-legend('ONTOS','LONGS','LONGN','CRANB','IBAYN','Sand')
+plot(ROIstat(:,1),ROIstat(:,2:end-1),'LineWidth',lw)
+legend('ONTOS','LONGS','LONGN','CRANB','IBAYN')
 xlabel('wavelength [\mum]','fontsize',fs)
-ylabel('Rrs [1/\pi]','fontsize',fs)
+ylabel('Rrs [1/sr]','fontsize',fs)
 set(gca,'fontsize',fs)
 xlim([.4 2.5]) 
 grid on
